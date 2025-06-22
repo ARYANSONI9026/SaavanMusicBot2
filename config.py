326586f541b6cef0aff7286b38181e021b771f7c
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-06-22 12:19:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,9 +83,9 @@ STICKERS = [
     "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ"
 ]
 HELP_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
-PING_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+PING_VID_URL = "https://files.catbox.moe/3ivvgo.mp4"
 PLAYLIST_IMG_URL = "https://telegra.ph/file/94e9eca3b0ec6e2dc6cd5.png"
-STATS_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+STATS_VID_URL = "https://files.catbox.moe/3ivvgo.mp4"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/mlztag.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/tiss2b.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/1d3da7.jpg"
