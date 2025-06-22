55bf579247dec7a93aefcc65d9c4143ca4811444
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-06-22 13:30:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -73,9 +73,9 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "2500"))
 # ───── Bot Media Assets ───── #
 
 START_VIDS = [
-    "https://i.ibb.co/sdXm8ZdF/Img2url-bot.jpg",
-    "https://i.ibb.co/sdXm8ZdF/Img2url-bot.jpg",
-    "https://i.ibb.co/sdXm8ZdF/Img2url-bot.jpg"
+    "https://files.catbox.moe/av5o3h.mp4",
+    "https://files.catbox.moe/av5o3h.mp4",
+    "https://files.catbox.moe/av5o3h.mp4"
 ]
 
 STICKERS = [
@@ -83,9 +83,9 @@ STICKERS = [
     "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ"
 ]
 HELP_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
-PING_VID_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+PING_VID_URL = "https://files.catbox.moe/1zfcm5.mp4"
 PLAYLIST_IMG_URL = "https://telegra.ph/file/94e9eca3b0ec6e2dc6cd5.png"
-STATS_VID_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+STATS_VID_URL = "https://files.catbox.moe/d03zlc.mp4"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/mlztag.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/tiss2b.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/1d3da7.jpg"
